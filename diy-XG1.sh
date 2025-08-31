2624ed4ffad05849f583d0b2fb0db8c6db8cd0c4
0012h <84757505+0012h@users.noreply.github.com>
2025-08-31 16:34:28 +0800

    Update diy-XG1.sh

--- diy-XG1.sh
+++ diy-XG1.sh
@@ -8,7 +8,15 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
-#
+
+# 更新并安装所有feeds
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+# 特别安装PassWall相关的包
+./scripts/feeds install -a -f -p passwall_packages
+./scripts/feeds install luci-app-passwall
+
 
 # 1.修改默认ip
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
@@ -27,6 +35,4 @@ sed -i 's/\/bin\/login/\/bin\/login -f root/' /etc/config/ttyd
 
 # 7.修正连接数（by ベ七秒鱼ベ）
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
-
-
 
