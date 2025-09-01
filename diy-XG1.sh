4a4f68f8277cd6c9e61ea62056772e54c8abc49c
0012h <84757505+0012h@users.noreply.github.com>
2025-09-01 08:41:34 +0800

    Update diy-XG1.sh

--- diy-XG1.sh
+++ diy-XG1.sh
@@ -9,15 +9,6 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 
-# 更新并安装所有feeds
-./scripts/feeds update -a
-./scripts/feeds install -a
-
-# 特别安装PassWall相关的包
-./scripts/feeds install -a -f -p passwall_packages
-./scripts/feeds install luci-app-passwall
-
-
 # 1.修改默认ip
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
